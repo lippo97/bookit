1e6c596620480d33ed659763dcc0c9cc3aa994be
Lippo <filippo.nardini@studio.unibo.it>
2021-09-23 19:49:43 +0200

    Remove user info stub data

--- packages/app/src/components/Layout/DrawerUserInfo.tsx
+++ packages/app/src/components/Layout/DrawerUserInfo.tsx
@@ -3,6 +3,7 @@ import { styled } from '@material-ui/core/styles';
 
 import { orange, red, pink, blue, green } from '@material-ui/core/colors';
 import { BooleanArraySupportOption } from 'prettier';
+import { useIsLoggedIn } from '@/stores/authentication';
 
 const Name = styled(Typography)(({ theme }) => ({
   fontSize: '1.1em',
@@ -21,23 +22,23 @@ const Layout = ({
 }: {
   avatar: React.ReactNode;
   name: string;
-  email: string;
+  email?: string;
 }) => (
   <Box p={2} pt={1}>
     <Box mb={2}>{avatar}</Box>
     <Name>{name}</Name>
-    <Email>{email}</Email>
+    {email && <Email>{email}</Email>}
   </Box>
 );
 export const DrawerUserInfo = () => {
   const colors = [green[500], blue[500], pink[500], orange[500], red[500]];
   const email = 'mario@rossi.com';
-  const isLoggedIn = false as boolean;
+  const isLoggedIn = useIsLoggedIn();
 
   const n = email.charCodeAt(0) % colors.length;
 
   if (!isLoggedIn) {
-    return <Layout avatar={<Avatar>?</Avatar>} name="Not signed in" email="" />;
+    return <Layout avatar={<Avatar>?</Avatar>} name="Not signed in" />;
   }
 
   return (
