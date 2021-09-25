400984ad753d92d1b2a9f8efcd79d0779b038067
Filippo Nardini <filippo.nardini@studio.unibo.it>
2021-09-25 21:35:43 +0200

    Distinguish between simple and manager account

--- packages/shared/src/data/managerAccount.ts
+++ packages/shared/src/data/managerAccount.ts
@@ -3,10 +3,7 @@ import { accountTypes } from '../types/account';
 
 export const ManagerAccountSchema = Joi.object({
   email: Joi.string().required(),
-  firstName: Joi.string().required(),
-  secondName: Joi.string().required(),
-  birthDate: Joi.date().required(),
-  maleFemale: Joi.valid('male', 'female', 'other').required(),
+  businessName: Joi.string().required(),
   type: Joi.string().valid(accountTypes.manager).required(),
 }).meta({
   className: 'ManagerAccount',
--- packages/shared/src/types/account.ts
+++ packages/shared/src/types/account.ts
@@ -1,4 +1,15 @@
+import { UserAccount, ManagerAccount } from '../generatedTypes';
+import { Account } from '../generatedTypes/authentication';
+
 export const accountTypes = {
   manager: 'manager',
   user: 'user',
 } as const;
+
+export function isUserAccount(account: Account): account is UserAccount {
+  return account.type === 'user';
+}
+
+export function isManagerAccount(account: Account): account is ManagerAccount {
+  return !isUserAccount(account);
+}
