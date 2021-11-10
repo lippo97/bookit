3a18438c507e79d7b43a1fcc86e4ed2d5c92aa08
Lippo <filippo.nardini@studio.unibo.it>
2021-11-10 15:43:26 +0100

    Mobile sidebar working, with some glitch

--- packages/app/src/features/dashboard/components/FloorMap/Sidebar.tsx
+++ packages/app/src/features/dashboard/components/FloorMap/Sidebar.tsx
@@ -1,46 +1,48 @@
 import { useMobile } from '@/hooks/useMobile';
 import { useOpenClose } from '@/hooks/useOpenClose';
-import { Box, IconButton, Paper, Theme } from '@material-ui/core';
+import { Box, IconButton, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 // These two are inverted
 import ExpandMoreIcon from '@material-ui/icons/ExpandLess';
 import ExpandLessIcon from '@material-ui/icons/ExpandMore';
 import clsx from 'clsx';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useSeats } from '../../stores/seats';
 import { SidebarContent } from './SidebarContent';
 
-interface SidebarProps {}
-
 const sidebarWidth = 256;
 
-type StyleProps = {
-  open: boolean;
-};
-
 const useMobileStyles = makeStyles((theme) => ({
-  mobile: {
+  container: {
     position: 'absolute',
-    transition: 'translate 0.3s, opacity 0.3s',
     left: 0,
     bottom: 0,
     right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  containerOpen: {
+    top: 0,
+    height: '100%',
+    zIndex: 1400,
+    overflowY: 'scroll',
+  },
+  mobile: {
+    transition: 'translate 0.3s, opacity 0.3s, height 0.3s',
     padding: theme.spacing(1.5),
-    zIndex: theme.zIndex.drawer,
     '& > *': {
       marginBottom: theme.spacing(2),
     },
-    width: '100%',
     height: '560px',
     translate: `0 ${560 - 40 - theme.spacing(1.5)}px`,
     transformOrigin: 'top left',
     borderRadius: theme.spacing(1),
-    opacity: 0.5,
+    opacity: 0.3,
   },
   mobileOpen: {
     translate: '0 0',
     opacity: 1,
-    zIndex: 1400,
+    height: 'auto',
   },
 }));
 
@@ -78,29 +80,35 @@ const Mobile = () => {
   }, [open, selectedIds]);
 
   return (
-    <Paper
-      square
-      elevation={3}
-      className={clsx(classes.mobile, { [classes.mobileOpen]: open })}
-    >
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        height={40}
+    <div className={clsx(classes.container, {[classes.containerOpen]: open})}>
+        <Box flex={1} />
+      <Paper
+        square
+        elevation={3}
+        className={clsx(classes.mobile, { [classes.mobileOpen]: open })}
       >
-        {open ? (
-          <IconButton onClick={handleClose}>
-            <ExpandLessIcon />
-          </IconButton>
-        ) : (
-          <IconButton onClick={handleOpen} disabled={selectedIds.length === 0}>
-            <ExpandMoreIcon />
-          </IconButton>
-        )}
-      </Box>
-      <SidebarContent />
-    </Paper>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height={40}
+        >
+          {open ? (
+            <IconButton onClick={handleClose}>
+              <ExpandLessIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              onClick={handleOpen}
+              disabled={selectedIds.length === 0}
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          )}
+        </Box>
+        <SidebarContent />
+      </Paper>
+    </div>
   );
 };
 
