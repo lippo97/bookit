8d647debb20d687220cba41e1b0a9335b5b07f97
Filippo <filippo.nardini@studio.unibo.it>
2022-08-24 17:46:53 +0200

    Better Update in resources

--- packages/resources/src/routes/operations/Update.ts
+++ packages/resources/src/routes/operations/Update.ts
@@ -1,14 +1,11 @@
-import { Error } from '@asw-project/shared/errors';
+import { Error, unexpectedError } from '@asw-project/shared/errors';
+import { NotFoundKind } from '@asw-project/shared/errors/kinds';
 import { ObjectId } from 'mongodb';
 import { Document, UpdateWithAggregationPipeline } from 'mongoose';
-import { EitherAsync } from 'purify-ts';
+import { EitherAsync, Left, Right } from 'purify-ts';
 import { AbstractService } from '../AbstractService';
 import { DocumentCreationError } from './documentCreation';
-import {
-  FindByIdError,
-  ProtectedFindByIdOptions,
-  SimpleFindById,
-} from './FindById';
+import { FindByIdError, ProtectedFindByIdOptions } from './FindById';
 
 export type EditError = DocumentCreationError | FindByIdError;
 export type UpdateParam = UpdateWithAggregationPipeline;
@@ -35,8 +32,29 @@ export const updateDocument =
         .exec(),
     );
 
-export class SimpleUpdate<T> extends SimpleFindById<T> implements Update<T> {
+export class SimpleUpdate<T> extends AbstractService<T> implements Update<T> {
+  // eslint-disable-next-line no-underscore-dangle
+  _checkNotFound(value: T | null): EitherAsync<Error<NotFoundKind>, T> {
+    if (value) {
+      return EitherAsync.liftEither(Right(value));
+    }
+    return EitherAsync.liftEither(Left({ kind: 'NotFound' } as const));
+  }
+
   update(id: ObjectId, update: UpdateParam): EitherAsync<Error<EditError>, T> {
-    return this.findById(id).chain(updateDocument(update));
+    return (
+      EitherAsync(() =>
+        this.model.findOneAndUpdate({ _id: id } as any, update, {
+          new: true,
+          runValidators: true,
+        }),
+      )
+        // eslint-disable-next-line no-underscore-dangle
+        .chain(this._checkNotFound)
+        .mapLeft((err: any) => {
+          console.log(err);
+          return unexpectedError(err);
+        })
+    );
   }
 }
