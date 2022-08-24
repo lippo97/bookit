f313c196c25bbddee68b8bb36d02e1a96d1109d1
Filippo Nardini <filippo.nardini@studio.unibo.it>
2022-08-24 16:51:22 +0200

    Checkpoint

--- packages/app/src/features/dashboard/routes/ConfirmReservation.tsx
+++ packages/app/src/features/dashboard/routes/ConfirmReservation.tsx
@@ -1,19 +1,12 @@
 import { StepLayout } from '@/components/StepLayout';
-import {
-  Box,
-  CircularProgress,
-  Container,
-  Paper,
-  Typography,
-} from '@material-ui/core';
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
-import CropFreeIcon from '@material-ui/icons/CropFree';
-import CrossIcon from '@material-ui/icons/Clear';
-import LoadingIcon from '@material-ui/icons/HourglassEmpty';
+import { useNotification } from '@/stores/notifications';
+import { Box, Container, Paper, Typography } from '@material-ui/core';
 import CameraIcon from '@material-ui/icons/Camera';
+import CropFreeIcon from '@material-ui/icons/CropFree';
+import { HTTPError } from 'ky';
+import { FC, useRef, useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 import { useMutation } from 'react-query';
-import { useNotification } from '@/stores/notifications';
 import { confirmReservation } from '../api/reservations';
 
 type State =
@@ -22,9 +15,10 @@ type State =
   | 'scanning'
   | 'fetching'
   | 'confirmed'
+  | 'already_confirmed'
   | 'rejected';
 
-const throttleTime = 5 * 1000;
+const throttleTime = 3 * 1000;
 
 const ScannerPlaceholder: FC = () => (
   <Box
@@ -44,97 +38,116 @@ const ScannerPlaceholder: FC = () => (
 );
 
 const ScanOverlay: FC<{ state: State }> = ({ state }) => {
-  const style = {
+  const baseStyle = {
     position: 'absolute',
     width: 200,
     height: 200,
     top: 25,
     left: 25,
   } as const;
-  switch (state) {
-    case 'scanning':
-      return <CropFreeIcon style={style} />;
-    case 'rejected':
-      return <CrossIcon style={style} />;
-    case 'fetching':
-      return <LoadingIcon style={style} />;
-    case 'confirmed':
-      return <CropFreeIcon style={{ ...style, color: 'green' }} />;
-    default:
-      return <></>;
-  }
+  /* eslint-disable no-nested-ternary */
+  const color =
+    state === 'scanning'
+      ? '#000000aa'
+      : state === 'rejected'
+      ? '#75020278'
+      : state === 'fetching'
+      ? '#00000044'
+      : state === 'confirmed'
+      ? '#00600f78'
+      : '#00000000';
+  /* eslint-enable no-nested-ternary */
+  return <CropFreeIcon style={{ ...baseStyle, color }} />;
 };
 
 const Message: FC<{ state: State }> = ({ state }) => {
-  switch (state) {
-    case 'scanning':
-      return (
-        <Typography variant="body1">
-          Frame the QR code into the camera.
-        </Typography>
-      );
-    case 'fetching':
-      return <Typography variant="body1">Checking...</Typography>;
-    default:
-      return <></>;
-  }
+  const caseOf = (state: State) => {
+    switch (state) {
+      case 'already_confirmed':
+        return 'Reservation was already confirmed.';
+      case 'bad_device':
+        return 'Unable to access the camera. Check your permissions.';
+      case 'confirmed':
+        return 'Reservation confirmed successfully!';
+      case 'fetching':
+        return 'Checking validity...';
+      case 'rejected':
+        return 'Unable to confirm reservation. Retry later.';
+      case 'scanning':
+        return 'Frame the QR code into the camera.';
+      default:
+        return '';
+    }
+  };
+  return <Typography variant="body1">{caseOf(state)}</Typography>;
 };
 
 export const ConfirmReservation: FC = () => {
   const state = useRef<State>('initial');
   const [stateStr, setStateStr] = useState<State>(state.current);
+  const setState = (updated: State) => {
+    state.current = updated;
+    setStateStr(updated);
+  };
   const { pushNotification } = useNotification();
-  const { mutateAsync, status } = useMutation<void, string, string>(
+  const { mutateAsync } = useMutation<void, string, string>(
     'reservation/confirm',
     confirmReservation,
-    {
-      onSuccess: () => {
-        pushNotification({
-          message: 'Reservation confirmed successfully.',
-          severity: 'success',
-        });
-      },
-      onError: () => {
-        pushNotification({
-          message: 'Unable to confirm reservation.',
-          severity: 'error',
-        });
-      },
-    },
   );
 
   const sleep = (timer: number) =>
     new Promise((resolve) => setTimeout(resolve, timer));
 
-  const onResult = async (
-    readData: { getText(): string } | undefined | null,
-    error: Error | undefined | null,
-  ) => {
+  const checkPermissions = (error: Error | undefined | null): void => {
     if (state.current === 'initial') {
-      if (error) {
-        console.error(error.name);
+      if (
+        error?.name === 'NotAllowedError' ||
+        error?.name === 'NotFoundError'
+      ) {
         pushNotification({
           severity: 'error',
           message: 'Unable to start the camera',
         });
-        state.current = 'bad_device';
-        return setStateStr('bad_device');
+        return setState('bad_device');
       }
-      state.current = 'scanning';
-      return setStateStr('scanning');
+      return setState('scanning');
     }
+  };
+
+  const onResult = async (
+    readData: { getText(): string } | undefined | null,
+    error: Error | undefined | null,
+  ): Promise<void> => {
+    checkPermissions(error);
     if (state.current === 'scanning') {
       const data = readData?.getText();
       if (data) {
-        state.current = 'fetching';
-        setStateStr('fetching');
-        await Promise.all([sleep(2000), Promise.resolve()]);
-        state.current = 'confirmed';
-        setStateStr('confirmed');
-        setTimeout(() => {
-          state.current = 'scanning';
-          setStateStr('scanning');
-        }, throttleTime);
+        setState('fetching');
+        try {
+          await Promise.all([sleep(1000), mutateAsync(data)]);
+          setState('confirmed');
+          pushNotification({
+            message: 'Reservation confirmed successfully.',
+            severity: 'success',
+          });
+        } catch (err) {
+          if (err instanceof HTTPError && err.response.status === 400) {
+            setState('already_confirmed');
+            pushNotification({
+              message: 'Reservation was already confirmed.',
+              severity: 'warning',
+            });
+          } else {
+            pushNotification({
+              message: 'Unable to confirm reservation. Retry later.',
+              severity: 'error',
+            });
+          }
+        } finally {
+          setTimeout(() => {
+            setState('scanning');
+          }, throttleTime);
+        }
       }
     }
   };
